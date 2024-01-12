255b75c19e3bc76d78c25488cf37c04b345cc280
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-01-12 11:32:47 -0500

    updated other to friend

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function createPerson(name, age) {
     };
   }
   
-  function addPersonAsFriend(person, other) {
-    person.friends.push(other);
+  function addPersonAsFriend(person, friend) {
+    person.friends.push(friend);
     return person;
   }
