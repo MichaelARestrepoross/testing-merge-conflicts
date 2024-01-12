78a63e4d751c0ae5f71b8e8f8ff68416dfca43cb
Carlitos <carlitosdutan@pursuit.org>
2024-01-12 13:43:01 -0500

    carlitos first commit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function createPerson(name, age) {
-    return {
-      name,
-      age,
-      friends: [],
-    };
-  }
-  
-  function addPersonAsFriend(personOne, friendB) {
-    personOne.friends.push(friendB);
-    return personOne;
-  }
+  return {
+    name,
+    age,
+    friends: [],
+  };
+}
 
-  function fn(...c) {
-    if (!c.every((a) => Number.isNumber(a)))
-      throw "All arguments must be numbers.";
-    return c.reduce((a, b) => a + b);
-  }
-  
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
+function addPersonAsFriend(personOne, friendB) {
+  personOne.friends.push(friendB);
+  return personOne;
+}
+
+function fn(...c) {
+  if (!c.every((a) => Number.isNumber(a)))
+    throw "All arguments must be numbers.";
+  return c.reduce((previousVal, currentVal) => previousVal + currentVal);
+}
+
+fn(1, 2, 3); //> 6
+fn(10, "B", 20); //> error All arguments must be numbers.
